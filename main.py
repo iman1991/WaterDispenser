826ad29f7b95b56b2967eb97ca323d2d2bcb2ad0
Iman <iman1991@mail,ru>
2017-07-07 15:41:18 +0300

    Uart lib

--- main.py
+++ main.py
@@ -1,39 +1,60 @@
+import threading
 import socket
 import json
 import time
+import argparse
+import uartcontrol
+
+
 sock = socket.socket()
 
-devInfo = {
-    "idv": 1,
-    "state": "NO_WATER",
-    "input10Counter ": 0,
-    "out10Counter": 0,
-    "milLitlose": 0,
-    "milLitWentOut": 0,
-    "milLitContIn": 0,
-    "waterPrice": 0,
-    "containerMinVolume": 0,
-    "contVolume": 0,
-    "totalPaid": 0,
-    "sessionPaid": 0,
-    "leftFromPaid": 0,
-    "container": "TOO_LOW",
-    "currentContainerVolume": 0,
-    "consumerPump": False,
-    "mainPump": False,
-    "magistralPressure": False,
-    "mainValve": False,
-    "filterValve": False,
-    "washFilValve": False,
-    "tumperMoney": False,
-    "tumperDoor": False,
-    "serviceButton": False,
-    "freeBattom": False,
-    "Voltage": 0
-}
+
+
+
+
+
+# devInfo = {
+#     "idv": 1,
+#     "state": "NO_WATER",
+#     "input10Counter ": 0,
+#     "out10Counter": 0,
+#     "milLitlose": 0,
+#     "milLitWentOut": 0,
+#     "milLitContIn": 0,
+#     "waterPrice": 0,
+#     "containerMinVolume": 0,
+#     "contVolume": 0,
+#     "totalPaid": 0,
+#     "sessionPaid": 0,
+#     "leftFromPaid": 0,
+#     "container": "TOO_LOW",
+#     "currentContainerVolume": 0,
+#     "consumerPump": False,
+#     "mainPump": False,
+#     "magistralPressure": False,
+#     "mainValve": False,
+#     "filterValve": False,
+#     "washFilValve": False,
+#     "tumperMoney": False,
+#     "tumperDoor": False,
+#     "serviceButton": False,
+#     "freeBattom": False,
+#     "Voltage": 0
+# }
+
+
 
 def connect():
-    sock.connect(("192.168.10.32", 9090))
+    sock.connect(("192.168.10.32", 8080))
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--baud", nargs="?", type=int, default=38400,
+                        help="скорость передачи данных, по умолчанию равно 38400")
+    parser.add_argument("-p", "--port", nargs="?", default="/dev/ttyUSB0",
+                        help="порт по которому опрашивается устройство")
+    return parser
 
 
 def send(info, method="status"):
@@ -49,27 +70,24 @@ def seans(info):
     send(info, method="connect")
     while True:
         data = sock.recv(2048).decode()
-
         if not data:
             raise IOError
-        if data != {"method": "status", "param": "saved"}:
-            print("response '%s'" % data)
         response = json.loads(data)
         method = response["method"]
         param = response["param"]
-        # time.sleep(1)
+        print("response %s" % data)
+        time.sleep(1)
         if method == "GetWater":
-            if param["idv"] == info["idv"] and param["balance"] > 0.1:
-                send(info)
+            if int(param["idv"]) == info["idv"]:
+                pass
         elif method == "ToUpBalans":
-            if param["idv"] == info["idv"] and param["balance"] > 0.1:
-                send(info)
-        elif method == "GetInfo":
-            send(info)
-        else:
-            send(info)
+            if int(param["idv"]) == info["idv"]:
+                pass
+        send(info)
 
 
 
-seans(devInfo)
+if __name__ == "__main__":
+
+    seans(uartcontrol.devInfo)
 
--- uartcontrol.py
+++ uartcontrol.py
@@ -1,40 +1,12 @@
 import serial
-import argparse
 import json
 import time
-import threading
 
 
-devInfo = {
-    "idv": 1,
-    "state": "NO_WATER",
-    "input10Counter ": 0,
-    "out10Counter": 0,
-    "milLitlose": 0,
-    "milLitWentOut": 0,
-    "milLitContIn": 0,
-    "waterPrice": 0,
-    "containerMinVolume": 0,
-    "contVolume": 0,
-    "totalPaid": 0,
-    "sessionPaid": 0,
-    "leftFromPaid": 0,
-    "container": "TOO_LOW",
-    "currentContainerVolume": 0,
-    "consumerPump": False,
-    "mainPump": False,
-    "magistralPressure": False,
-    "mainValve": False,
-    "filterValve": False,
-    "washFilValve": False,
-    "tumperMoney": False,
-    "tumperDoor": False,
-    "serviceButton": False,
-    "freeBattom": False,
-    "Voltage": 0
-}
+
 
 ID = -1
+
 
 input10Counter = 0
 out10Counter = 1
@@ -45,143 +17,171 @@ waterPrice = 5
 containerMinVolume = 6
 maxContainerVolume = 7
 totalPaid = 8
-# sessionPaid = 9
-leftFromPaid = 9
-state = 10
-container = 11
-currentContainerVolume = 12
-consumerPump = 13
-mainPump = 14
-magistralPressure = 15
-mainValve = 16
-filterValve = 17
-washFilValve = 18
-tumperMoney = 19
-tumperDoor = 20
-serviceButton = 21
-freeBattom = 22
-Voltage = 23
+sessionPaid = 9
+leftFromPaid = 10
+state = 11
+container = 12
+currentContainerVolume = 13
+consumerPump = 14
+mainPump = 15
+magistralPressure = 16
+mainValve = 17
+filterValve = 18
+washFilValve = 19
+tumperMoney = 20
+tumperDoor = 21
+serviceButton = 22
+freeBattom = 23
+Voltage = 24
 
 
 GET_INFO = b'g\n'
 SETTING = 'cs'
+PUTTING = b'cm'
+
 NOT_ERROR = b'0\n'
-ERROR_ASCII = b'1\n'
-ERROR_SHORT = b'2\n'
-ERROR_LONG = b'3\n'
+ERROR_ASCII = b'-1\n'
+ERROR_SHORT = b'-2\n'
+ERROR_LONG = b'-3\n'
+ERROR_TEST = b'-4\n'
 
 
-stateList =["NO_WATER",
-            "WASH_FILTER",
-            "WAIT",
-            "SETTING",
-            "JUST_PAID",
-            "WORK",
-            "SERVICE",
-            "FREE",
-            "NONE"
-            ]
-
-
+stateList = ["NO_WATER", "WASH_FILTER", "WAIT", "SETTING", "JUST_PAID", "WORK", "SERVICE", "FREE", "NONE"]
 containerList = ["TOO_LOW", "NOT_FULL", "FULL"]
 
-def testTime():
-    i = 0
-    while True:
-        time.sleep(1)
-        i += 1
-        print("Seconds %i" % i)
+
+class vodomat(object):
+
+    devInfo = {
+        "idv": 1,
+        "state": "NO_WATER",
+        "input10Counter ": 0,
+        "out10Counter": 0,
+        "milLitlose": 0,
+        "milLitWentOut": 0,
+        "milLitContIn": 0,
+        "waterPrice": 0,
+        "containerMinVolume": 0,
+        "contVolume": 0,
+        "totalPaid": 0,
+        "sessionPaid": 0,
+        "leftFromPaid": 0,
+        "container": "TOO_LOW",
+        "currentContainerVolume": 0,
+        "consumerPump": False,
+        "mainPump": False,
+        "magistralPressure": False,
+        "mainValve": False,
+        "filterValve": False,
+        "washFilValve": False,
+        "tumperMoney": False,
+        "tumperDoor": False,
+        "serviceButton": False,
+        "freeBattom": False,
+        "Voltage": 0
+    }
 
 
-def checkCode(code):
-    if code == NOT_ERROR:
-        return True
+    def __init__(self, port, baud):
+        self.uart = serial.Serial(port, baud)
+        self.lock = False
 
-    elif code ==ERROR_ASCII:
-        raise IOError("Неверный ASCII Символ")
-
-    elif code ==ERROR_SHORT:
-        raise IOError("Слишкм короткая строка")
-
-    elif code ==ERROR_LONG:
-        raise IOError("Слишкм длинная строка")
-
-    else:
-        raise IOError(code)
-
-
-
-def read(com):
-    code = com.readline()
-    if checkCode(code):
-        return com.readline()
-
-
-def readinfo(com):
-    com.write(GET_INFO)
-    return read(com)
-
-
-def setting(com,_waterPrice, _containerMinVolume,_maxContainerVolume):
-    msg = "{}{},{},{}\n".format(SETTING, _waterPrice, _containerMinVolume, _maxContainerVolume).encode("utf-8")
-    com.write(msg)
-    print(msg)
-    raw = com.readline()
-    checkCode(raw)
-
-
-
-
-def raw2list(raw):
-    date = json.loads(raw.decode())
-    dev = {}
-    dev["input10Counter"] = date[input10Counter]
-    dev["out10Counter"] = date[out10Counter]
-    dev["milLitlose"] = date[milLitlose]
-    dev["milLitWentOut"] = date[milLitWentOut]
-    dev["milLitContIn"] = date[milLitContIn]
-    dev["waterPrice"] = date[waterPrice]
-    dev["containerMinVolume"] = date[containerMinVolume]
-    dev["maxContainerVolume"] = date[maxContainerVolume]
-    dev["totalPaid"] = date[totalPaid]
-    # dev["sessionPaid"] = date[sessionPaid]
-    dev["leftFromPaid"] = date[leftFromPaid]
-    dev["state"] = stateList[date[state]]
-    dev["container"] = containerList[date[container]]
-    dev["currentContainerVolume"] = date[currentContainerVolume]
-    dev["consumerPump"] = date[consumerPump] == 1
-    dev["mainPump"] = date[mainPump] == 1
-    dev["magistralPressure"] = date[magistralPressure] == 1
-    dev["mainValve"] = date[mainValve] == 1
-    dev["filterValve"] = date[filterValve] == 1
-    dev["washFilValve"] = date[washFilValve] == 1
-    dev["tumperMoney"] = date[tumperMoney] == 1
-    dev["tumperDoor"] = date[tumperDoor] == 1
-    dev["serviceButton"] = date[serviceButton] == 1
-    dev["freeBattom"] = date[freeBattom] == 1
-    dev["Voltage"] = date[Voltage]
-    return dev
-
-
-def createParser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--baud", nargs="?", type=int, default=38400,
-                        help="скорость передачи данных, по умолчанию равно 38400")
-    parser.add_argument("-p", "--port", nargs="?", default="/dev/ttyUSB0",
-                        help="порт по которому опрашивается устройство")
-    return parser
-
-def main():
-    parser = createParser()
-    namespace = parser.parse_args()
-    port = serial.Serial(namespace.port, namespace.baud)
-    raw = readinfo(port)
-    devInfo.update(raw2list(raw))
-    print(json.dumps(devInfo))
+    def checkCode(self, code, types="code"):
+        if code == NOT_ERROR:
+            return True
+        elif code ==ERROR_ASCII:
+            raise IOError("Неверный ASCII Символ")
+        elif code ==ERROR_SHORT:
+            raise IOError("Слишкм короткая строка")
+        elif code ==ERROR_LONG:
+            raise IOError("Слишкм длинная строка")
+        elif code == ERROR_TEST:
+            return False
+        else:
+            if types == "code":
+                raise IOError(code)
+            elif types == "int":
+                return int(code)
 
 
 
-if __name__ == "__main__":
-    main()
+    def read(self):
+        code = self.uart.readline()
+        if self.checkCode(code):
+            return self.uart.readline()
+
+
+    def readinfo(self):
+        while self.lock:
+            pass
+        self.lock = True
+        self.uart.write(GET_INFO)
+        return self.read()
+
+
+
+
+
+    def raw2list(self, raw):
+        date = json.loads(raw.decode())
+        self.devInfo["input10Counter"] = date[input10Counter]
+        self.devInfo["out10Counter"] = date[out10Counter]
+        self.devInfo["milLitlose"] = date[milLitlose]
+        self.devInfo["milLitWentOut"] = date[milLitWentOut]
+        self.devInfo["milLitContIn"] = date[milLitContIn]
+        self.devInfo["waterPrice"] = date[waterPrice]
+        self.devInfo["containerMinVolume"] = date[containerMinVolume]
+        self.devInfo["maxContainerVolume"] = date[maxContainerVolume]
+        self.devInfo["totalPaid"] = date[totalPaid]
+        self.devInfo["sessionPaid"] = date[sessionPaid]
+        self.devInfo["leftFromPaid"] = date[leftFromPaid]
+        self.devInfo["state"] = stateList[date[state]]
+        self.devInfo["container"] = containerList[date[container]]
+        self.devInfo["currentContainerVolume"] = date[currentContainerVolume]
+        self.devInfo["consumerPump"] = date[consumerPump] == 1
+        self.devInfo["mainPump"] = date[mainPump] == 1
+        self.devInfo["magistralPressure"] = date[magistralPressure] == 1
+        self.devInfo["mainValve"] = date[mainValve] == 1
+        self.devInfo["filterValve"] = date[filterValve] == 1
+        self.devInfo["washFilValve"] = date[washFilValve] == 1
+        self.devInfo["tumperMoney"] = date[tumperMoney] == 1
+        self.devInfo["tumperDoor"] = date[tumperDoor] == 1
+        self.devInfo["serviceButton"] = date[serviceButton] == 1
+        self.devInfo["freeBattom"] = date[freeBattom] == 1
+        self.devInfo["Voltage"] = date[Voltage]
+
+
+    def startUart(self):
+        while True:
+            raw = self.readinfo()
+            self.lock = False
+            self.raw2list(raw)
+            time.sleep(1)
+
+    def setting(self, _waterPrice, containerMinVolume,_maxContainerVolume):
+        msg = "{}{},{},{}\n".format(SETTING, _waterPrice, containerMinVolume, _maxContainerVolume).encode("ascii")
+        while self.lock:
+            pass
+        self.lock = True
+        self.uart.write(msg)
+        raw = self.uart.readline()
+        self.checkCode(raw)
+        self.lock = False
+
+    def getPutting(self):
+        while self.lock:
+            pass
+        self.lock = True
+        self.uart.write(PUTTING)
+        raw = self.uart.readline()
+        code = self.checkCode(raw, code="int")
+        if code:
+            return 0
+        elif code > 0:
+            return code
+        else:
+            raise IOError(raw)
+
+
 
 
