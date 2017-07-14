876cce715685fea04608e15a5aa2c32d7dc71759
iman1991 <iman1991@mail.ru>
2017-07-14 19:31:30 +0300

    new config

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import uartcontrol
 
 
 sock = socket.socket()
-dev = uartcontrol.Vodomat("COM4", 9600)
-
+dev = uartcontrol.Vodomat("/dev/ttyAMA0", 38400)
 
 def connect():
     sock.connect(("192.168.10.32", 9090))
