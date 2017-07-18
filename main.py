63dc14b866f35a66abd56e5e6552f4d5dc76ee86
Iman <iman1991@mail,ru>
2017-07-18 15:13:48 +0300

    print

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ dev = uartcontrol.Vodomat("/dev/ttyAMA0", 38400)
 
 
 def connect():
-    try:
-        sock.connect(("194.67.217.180", 8080))
-    except ConnectionRefusedError:
-        sock.connect(("194.67.217.180", 9090))
+    sock.connect(("194.67.217.180", 8080))
+    # try:
+    #     sock.connect(("194.67.217.180", 8080))
+    # except ConnectionRefusedError:
+    #     sock.connect(("194.67.217.180", 9090))
 
 def send(info, method="status"):
     d = {"method": method, "param": info}
