af4789ed7d0a96e3e5ecf603b5277f81bd5d3aee
Iman <iman1991@mail,ru>
2017-07-19 15:07:24 +0300

    zabbix action agent

--- agent.py
+++ agent.py
@@ -11,37 +11,40 @@ host = 'vodomat001'
 server = "194.67.217.17"
 port = 10051
 
-itemList = ItemList(host='vodomat001')
-response = itemList.get(server="194.67.217.17", port=10051)
-keys = response.data
+def getkey():
+    itemList = ItemList(host='vodomat001')
+    response = itemList.get(server="194.67.217.17", port=10051)
+    return response.data
 
-while True:
-    clock = int(time.time())
-    request = {
-        "request": "agent data",
-        "data":[],
-        "clock": clock
-    }
 
-    for item in keys:
-        try:
 
-            request["data"].append({"host": host,
-                                "key": item["key"],
-                                "value": dev.devInfo[item["key"]],
+def startAgent():
+    keys = [item["key"] for item in getkey()]
+    while True:
+        clock = int(time.time())
+        request = {
+            "request": "agent data",
+            "data":[],
+            "clock": clock
+        }
+        for key in keys:
+            try:
+                request["data"].append({"host": host,
+                                "key": key,
+                                "value": dev.devInfo[key],
                                 "clock": clock
                                 })
 
-        except:
-            print(item["key"])
-
-    raw = zbx.get_data_to_send(json.dumps(request))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("194.67.217.17", 10051))
-    sock.send(raw)
-    data = sock.recv(5)
-    if data == zbx.ZABBIX_HEADER:
-        data = sock.recv(8)
-        l = struct.unpack("i", data[:4])[0]
-        sock.close()
-    time.sleep(30)
+            except:
+                pass
+        raw = zbx.get_data_to_send(json.dumps(request))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("194.67.217.17", 10051))
+        sock.send(raw)
+        data = sock.recv(5)
+        if data == zbx.ZABBIX_HEADER:
+            data = sock.recv(8)
+            l = struct.unpack("i", data[:4])[0]
+            sock.recv(l)
+            sock.close()
+        time.sleep(30)
