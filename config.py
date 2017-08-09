b1482ec1e8f6598c69ce7c47071a00c0b2399150
iman1991 <iman1991@mail.ru>
2017-08-09 09:15:17 +0300

    add config file

--- config.py
+++ config.py
@@ -4,20 +4,17 @@ import os
 zabbix = {}
 uart = {}
 
-def openCofigFile():
-    config_file = open("/opt/dl_config.json")
-    raw = config_file.read()
-    config_file.close()
-    return raw
+config_file = open("/opt/dl_config.json")
 
+raw = config_file.read()
 
-try:
-    text = openCofigFile()
-except:
-    os.system("cp dl_config.json /opt/dl_config.json")
-    text = openCofigFile()
+config_file.close()
 
-config = json.loads(text)
+config = json.loads(raw)
+
 zabbix.update(config["zabbix"])
+
 uart.update(config["uart"])
 
+if __name__ == "__main__":
+    os.system("cp dl_config.json /opt/dl_config.json")
