0b1951c7f8b27c4c08434ce8882ff9a722dd3f3f
iman1991 <iman1991@mail.ru>
2017-09-14 10:21:15 +0300

    Starter config

--- config.py
+++ config.py
@@ -1,19 +1,21 @@
 import json
 import os
-zabbix = {}
-uart = {}
-
-config_file = open("/opt/dl_config.json")
-
-raw = config_file.read()
-
-config_file.close()
-
-config = json.loads(raw)
-
-zabbix.update(config["zabbix"])
-
-uart.update(config["uart"])
 
 if __name__ == "__main__":
     os.system("cp dl_config.json /opt/dl_config.json")
+else:
+    zabbix = {}
+    uart = {}
+
+    config_file = open("/opt/dl_config.json")
+
+    raw = config_file.read()
+
+    config_file.close()
+
+    config = json.loads(raw)
+
+    zabbix.update(config["zabbix"])
+
+    uart.update(config["uart"])
+
