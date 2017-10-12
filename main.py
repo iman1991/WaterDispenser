9268ccb198248eb12124e87813a8ad4e1899c4f3
iman1991 <iman1991@mail.ru>
2017-10-12 19:59:16 +0300

    Answer

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def report():
                     print("payment")
         elif command["cmd"] == 2:
             info = param
-            info["score"] = dev.devInfo["leftFromPaid"] // 10000
+            info["score"] = dev.devInfo["leftFromPaid"] // 100
             dev.getPutting()
             info["Status"] = dev.devInfo
             print(param["score"])
