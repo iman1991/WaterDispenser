c6554565c622c244628f554a3be29be877ab860a
iman1991 <iman1991@mail.ru>
2017-10-17 14:20:49 +0300

    no stop

--- uartcontrol.py
+++ uartcontrol.py
@@ -133,7 +133,8 @@ class Vodomat(object):
         self.devInfo["totalPaid"] = date[totalPaid]
         self.devInfo["sessionPaid"] = date[sessionPaid]
         self.devInfo["leftFromPaid"] = date[leftFromPaid]
-        self.devInfo["State"] = stateList[date[state]]
+        self.devInfo["State"] = "WAIT"
+        # self.devInfo["State"] = stateList[date[state]]
         self.devInfo["stateGraph"] = date[state]
         self.devInfo["container"] = containerList[date[container]]
         self.devInfo["containerGraph"] = date[container]
